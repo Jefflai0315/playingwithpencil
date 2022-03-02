1fe393bcf14e6fe8b94ba6d442a29859a6686965
Jefflai0315 <jefflai0315@gmail.com>
2022-03-02 18:56:08 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from turtle import st
+import streamlit as st
+import requests
+from streamlit_lottie import st_lottie
+
+
+st.set_page_config(page_icon="✏️", page_title="pwp")
+
+lottie_book = requests.get('https://assets4.lottiefiles.com/private_files/lf30_hqhmdw8f.json').json()
+st_lottie(lottie_book, speed=1, height=200, key="initial")
