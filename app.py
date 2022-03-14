52a14b7007c90b270b1c00238b1d7a5736044018
Jefflai0315 <jefflai0315@gmail.com>
2022-03-14 23:55:04 +0800

    remove art content from main app and create art.py

--- app.py
+++ app.py
@@ -13,18 +13,13 @@ import time
 from PIL import Image
 import app1
 import coffee
+import art
 # import TeachableMachine as tm
 
-
-PAGES = {
-    "App1": app1,
-}
 
 
 st.set_page_config(page_icon="✏️", page_title="pwp")
 
-lottie_art = requests.get('https://assets4.lottiefiles.com/private_files/lf30_hqhmdw8f.json').json()
-lottie_coffee = requests.get('https://assets9.lottiefiles.com/packages/lf20_urr8jb9p.json').json()
 # model, live, data, size = tm.init_face_reg()
 
 def main():
@@ -35,38 +30,13 @@ def main():
     if menu == 'Coffee':
         coffee.app()
         
-        
-
-    
-                    
-        
-        
-
-
-
-        
-    
-    #     line_chart = alt.Chart(df).mark_line().encode(x='Year',
-    # y='mean(Miles_per_Gallon)')
-    #     st.altair_chart(line_chart)
-
     
     if menu == 'Art':
-        
-        st_lottie(lottie_art, speed=1, height=200, key="Art")
-        st.subheader('GM GM, here are show of my recent artwork!')
-        st.markdown("I only do graphic pencil portrait at the moment, nice **_aren't_ they?**")
-        image1 = Image.open("https://github.com/Jefflai0315/playingwithpencil/blob/main/assets/images/photo.jpg")
-        st.image(image1,caption='Taeyeon',width=600)
-        image2= Image.open("https://github.com/Jefflai0315/playingwithpencil/blob/main/assets/images/photo1.jpg")
-        st.image(image2,caption='Momo',width=600)
-        image3= Image.open("https://github.com/Jefflai0315/playingwithpencil/blob/main/assets/images/photo2.jpg")
-        st.image(image3,caption='Dahyun',width=600)
+        art.app()
+    
 
     if menu == 'Secret':
-        result = tm.capture_face()
-        if result == True:
-            st.title('Hello jeff')
+        pass
     
     
 
