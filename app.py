40d4014a3992febe11404e4152dd0d1f89cd7b36
Jefflai0315 <jefflai0315@gmail.com>
2022-03-20 22:40:17 +0800

    ADD ALL CRED DATA

--- app.py
+++ app.py
@@ -75,9 +75,14 @@ if __name__ == "__main__":
             cred = credentials.Certificate({
             "type": "service_account",
             "project_id": "product-design-f47db",
+            "private_key_id": os.environ.get('PRIVATE_KEY_ID'),
             "private_key": os.environ.get('PRIVATE_KEY').replace('\\n', '\n'),
             "client_email": os.environ.get("CLIENT_EMAIL"),
-            "token_uri": "https://oauth2.googleapis.com/token"
+            "client_id": os.environ.get('CLIENT_ID'),
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url": os.environ.get('CLIENT_CERT_URL')
             })
 
             print(cred)
